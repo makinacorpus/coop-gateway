e68e63d6ccf7372dcc04f843d8d61bc3d5a34fc1
Antoine Cezar <antoine.cezar@makina-corpus.com>
2013-10-14 11:05:22 +0200

    Remove debug code

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import os
 
 from setuptools import setup, find_packages
-print(find_packages())
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
