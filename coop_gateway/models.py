4a9379d139be7271b502164bcd89b6e37c265e18
Antoine Cezar <antoine.cezar@makina-corpus.com>
2013-11-25 16:48:17 +0100

    Fix model dependencies imports

--- coop_gateway/models.py
+++ coop_gateway/models.py
@@ -1,19 +1,15 @@
 from django.db import models
-
-from coop_local.models import (
-    Organization,
-    Person,
-    Role,
-)
+from django.db.models.loading import get_model
 
 
 class ForeignOrganization(models.Model):
-    local_object = models.OneToOneField(Organization)
+    local_object = models.OneToOneField(get_model('coop_local',
+                                                  'Organization'))
 
 
 class ForeignPerson(models.Model):
-    local_object = models.OneToOneField(Person)
+    local_object = models.OneToOneField(get_model('coop_local', 'Person'))
 
 
 class ForeignRole(models.Model):
-    local_object = models.OneToOneField(Role)
+    local_object = models.OneToOneField(get_model('coop_local', 'Role'))
